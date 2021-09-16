b3dbeb1b1781d899043279278a4d9ee14fc4a46a
JacksonFrontEnd <evgenij_poposemov@mail.ru>
2021-09-16 14:39:49 +0300

    feat: add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Context } from ".";
 import "./App.scss";
-import Calendar from "./components/Calendar";
+import AppRouter from "./components/AppRouter";
+import Loader from "./components/Loader";
+import Navbar from "./components/Navbar";
 
 const App: React.FC = () => {
-  return <Calendar/>
+  const { auth } = useContext(Context);
+  const [user, loading, error] = useAuthState(auth);
+  return loading ?  <Loader/> :
+  (
+    <div>
+      <Navbar />
+      <AppRouter />
+    </div>
+  );
 };
 
 export default App;
